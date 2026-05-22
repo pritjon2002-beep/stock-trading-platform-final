eaff88ffe2284124c6ad293847fe9e2951661316
pritam-demo <pritjon2002@gmail.com>
2026-05-22 09:48:36 +0545

    fix project gitignore structure

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -10,13 +10,17 @@ import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
 
+import{ GeneralContextProvider} from "./GeneralContext";
+
+
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
+    <div className="dashboard-container d-flex">
       <GeneralContextProvider>
         <WatchList />
       </GeneralContextProvider>
-      <div className="content">
+      
+      <div className="content ms-auto">
         <Routes>
           <Route exact path="/" element={<Summary />} />
           <Route path="/orders" element={<Orders />} />
