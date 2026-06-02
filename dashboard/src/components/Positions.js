29e7a681aeaccddef78d380092da547d37c73e69
pritam-demo <pritjon2002@gmail.com>
2026-06-02 08:36:52 +0545

    fetch data from dashboard and displayed in  holdings and positions ,

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { positions } from "../data/data";
+// import { positions } from "../data/data";
+
+import axios from "axios";
 
 const Positions = () => {
+
+  const [allPositions, setallPositions] = useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:3002/allPositions").then((res)=>{
+      setallPositions(res.data);
+    });
+  },[]);
+
   return (
     <>
-      <h3 className="title">Positions ({positions.length})</h3>
+      <h3 className="title">Positions ({allPositions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -18,7 +28,7 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
-          {positions.map((stock, index) => {
+          {allPositions.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
