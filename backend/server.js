8743464e06ffdbc8b19b6abc11328cbd783d94b7
pritam-demo <pritjon2002@gmail.com>
2026-06-06 22:00:18 +0545

    user model imported signup and login route setup

--- backend/server.js
+++ backend/server.js
@@ -245,6 +245,90 @@ await newOrder.save();
 
 });
 
+//SIGNUP ROUTE
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await UserModel.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "Account created successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
+
+//LOGIN ROUTE
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Email",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.json({
+      token,
+      message: "Login Successful",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`App Started on port ${PORT}`);
 });
