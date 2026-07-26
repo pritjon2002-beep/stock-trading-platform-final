da0b5d1dd0a96f51057c829cc8055a6c60821f4f
pritam-demo <pritjon2002@gmail.com>
2026-07-26 17:52:53 +0545

    doughnout chart implemented

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -12,8 +12,39 @@ import {
 } from "@mui/icons-material";
 
 import { watchlist } from "../data/data";
+import { DoughnoutChart } from "./DoughnoutChart";
+
 
 const WatchList = () => {
+  
+  const labels = watchlist.map((stock) => stock.name);
+
+  const data  = {
+  labels,
+  datasets: [{
+    label: "Price",
+    data: watchlist.map((stock)=> stock.price),
+       backgroundColor: [
+        'rgba(255, 99, 132, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(255, 206, 86, 0.8)',
+        'rgba(75, 192, 192, 0.8)',
+        'rgba(153, 102, 255, 0.8)',
+        'rgba(255, 159, 64, 0.8)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    
+  }]
+}
+
   return (
     <div className="watchlist-container">
       <div className="search-container">
@@ -32,6 +63,9 @@ const WatchList = () => {
           return <WatchListItem stock={stock} key={index} />;
         })}
       </ul>
+
+<DoughnoutChart data ={data} />
+
     </div>
   );
 };
